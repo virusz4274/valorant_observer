49a31f041e9b4871f60907ca29953ec210eda03c
virusz4274 <43103921+virusz4274@users.noreply.github.com>
2023-04-14 12:28:39 +0530

    new trained model
    model has been trained use better data,
    screenshot segmentation processing has been added properly. 1080p monitor are good to go now

--- server-test/detect.py
+++ server-test/detect.py
@@ -10,35 +10,54 @@ def load_model(model_path):
     model = resnet50(weights=None)
     num_classes = 21
     model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
-    model.load_state_dict(torch.load(model_path))
+    model.load_state_dict(
+        torch.load(model_path, map_location=torch.device('cpu')))
     model.eval()
     return model
 
 
 # Preprocess the input image
-def preprocess_image(image_path):
+def preprocess_image(image):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
     ])
-    image = Image.open(image_path).convert('RGB')
     return transform(image).unsqueeze(0)
 
 
 # Predict the character name
-def predict_character_name(model, image_path, class_names):
-    input_tensor = preprocess_image(image_path)
+def predict_character_name_and_confidence(model, image, class_names):
+    input_tensor = preprocess_image(image)
     output = model(input_tensor)
     probabilities = torch.nn.functional.softmax(output, dim=1)
-    _, predicted_index = torch.max(probabilities, 1)
-    return class_names[predicted_index.item()]
+    max_prob, predicted_index = torch.max(probabilities, 1)
+    confidence = max_prob.item() * 100
+    return class_names[predicted_index.item()], confidence
 
 
 # Load the class names
 def load_class_names(dataset_folder):
     return sorted(os.listdir(dataset_folder))
+
+
+#split image into segments
+def split_image_into_segments(image, num_segments):
+    width, height = image.size
+    segment_width = width // num_segments
+    segments = []
+
+    for i in range(num_segments):
+        left = i * segment_width
+        top = 0
+        right = (i + 1) * segment_width
+        bottom = height
+        segment = image.crop((left, top, right, bottom))
+        segment.save(f"segment_{i}.png")
+        segments.append(segment)
+
+    return segments
 
 
 def capture_and_save_first_segment(output_dir):
@@ -47,19 +66,19 @@ def capture_and_save_first_segment(output_dir):
 
     # Calculate the size of each segment
     width, height = screenshot.size
-    segment_width = width // 8
+    segment_height = height // 10
 
     # Define the cropping coordinates for the first segment
     left = 0
     top = 0
-    right = segment_width
-    bottom = height
+    right = width
+    bottom = segment_height
 
     # Crop the first segment
     first_segment = screenshot.crop((left, top, right, bottom))
 
     # Save the first segment
-    output_path = os.path.join(output_dir, "first_segment.png")
+    output_path = os.path.join(output_dir, "agent_segment.png")
     first_segment.save(output_path)
 
 
@@ -67,13 +86,39 @@ def capture_and_save_first_segment(output_dir):
 if __name__ == "__main__":
     model_path = "character_classification_model.pth"
     data_dir = "./Agents/training"
-    image_path = "test.jpg"
+
+    output_dir = "./screenshots"
+    capture_and_save_first_segment(output_dir)
+
+    image_path = "./screenshots/agent_segment.png"
 
     model = load_model(model_path)
     class_names = load_class_names(data_dir)
-    character_name = predict_character_name(model, image_path, class_names)
+    # Load the input image and split it into segments
+    input_image = Image.open(image_path).convert('RGB')
+    num_segments = 27
+    segments = split_image_into_segments(input_image, num_segments)
 
-    output_dir = "path/to/output/directory"
-    capture_and_save_first_segment(output_dir)
+    # Predict the character's name for each segment
+    character_names_and_confidences = [
+        predict_character_name_and_confidence(model, segment, class_names)
+        for segment in segments
+    ]
 
-    print(f"The detected character is: {character_name}")
+    print("The detected characters are:")
+    start1 = 6
+    stop1 = 11
+    start2 = 16
+    stop2 = 21
+    for i, (character_name, confidence) in enumerate(
+            character_names_and_confidences[start1:stop1], start=start1):
+        if confidence >= 50:
+            print(f"{i}: {character_name} (Confidence: {confidence:.2f}%)")
+        else:
+            print(f"{i}: Not identified (Confidence: {confidence:.2f}%)")
+    for i, (character_name, confidence) in enumerate(
+            character_names_and_confidences[start2:stop2], start=start2):
+        if confidence >= 50:
+            print(f"{i}: {character_name} (Confidence: {confidence:.2f}%)")
+        else:
+            print(f"{i}: Not identified (Confidence: {confidence:.2f}%)")
